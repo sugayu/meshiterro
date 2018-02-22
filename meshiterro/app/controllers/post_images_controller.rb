089ba91a7e48fbca25452bd41547885320c9ed5e
YutaSugao <mrfeather@YutaSugaonoMacBook.local>
2018-02-22 16:52:35 +0900

    Finish 5-3

--- meshiterro/app/controllers/post_images_controller.rb
+++ meshiterro/app/controllers/post_images_controller.rb
@@ -1,18 +1,27 @@
 class PostImagesController < ApplicationController
   def new
-    
+    @post_image = PostImage.new
+
   end
 
   def create
-    
-  end
+   @post_image = PostImage.new(post_image_params)
+   @post_image.user_id = current_user.id
+   @post_image.save
+   redirect_to post_images_path
+ end
 
-  def index
-    
-  end
+ def index
+   @post_images = PostImage.all
+ end
 
-  def show
-    
-  end
-  
+ def show
+    @post_image = PostImage.find(params[:id])
+ end
+
+# 投稿画像についてのハッシュチェック
+private
+def post_image_params
+  params.require(:post_image).permit(:shop_name, :image, :caption, :user_id)
+end
 end
