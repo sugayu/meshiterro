835aefb44b31ab29bd89fef223980670d8854c76
YutaSugao <mrfeather@YutaSugaonoMacBook.local>
2018-02-24 15:47:08 +0900

    No method Error

--- meshiterro/config/routes.rb
+++ meshiterro/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'post_images#index'
   resources :post_images, only: [:new, :create, :index, :show]
+  resource :post_comments, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
