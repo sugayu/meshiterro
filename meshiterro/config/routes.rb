da89da2ae8125c3e30cbb35ae9f4b08d6551a30f
YutaSugao <mrfeather@YutaSugaonoMacBook.local>
2018-02-25 23:41:40 +0900

    Start 5-5

--- meshiterro/config/routes.rb
+++ meshiterro/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  resources :post_images, only: [:new, :create, :index, :show] do
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resource :post_comments, only: [:create, :destroy]
   end
